4801a16ee0c5d7d1ad875fa0ae6e7716a9863688
luonghuynh <luonghuynh2525@gmail.com>
2019-12-31 12:08:40 +0700

    commit file len

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    "use strict";
+
+  // cycle text contact
+  const contact_up = new CircleType(document.getElementById('contact-up')).radius(110);
+	const contact_sub = new CircleType(document.getElementById('contact-sub')).radius(110);
+});
