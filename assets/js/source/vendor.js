ae2e5a7992a1db90142d80bb7737e6e94fac91e3
luonghuynh <luonghuynh2525@gmail.com>
2020-01-10 14:32:38 +0700

    commit file len

---
+++ assets/js/source/vendor.js
@@ -0,0 +1,5 @@
+
+// @prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/transition.js
+// @ prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js
+// @ prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/tab.js
+// @prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/modal.js
